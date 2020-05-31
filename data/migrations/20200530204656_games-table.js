09316620935d76406832979e0d7bb4ccb6615bb6
Michael Harms <michael.harms6010@gmail.com>
2020-05-30 20:54:21 -0500

    adds ships table

--- data/migrations/20200530204656_games-table.js
+++ data/migrations/20200530204656_games-table.js
@@ -3,19 +3,19 @@ exports.up = function(knex) {
     return knex.schema.createTable("games", tbl => {
         tbl.increments();
 
-        tbl.string("p1_id", 64)
+        tbl.integer("p1_id", 64)
         .references("id")
         .inTable("users")
         .onDelete("CASCADE")
         .onUpdate("CASCADE")
 
-        tbl.string("p2_id", 64)
+        tbl.integer("p2_id", 64)
         .references("id")
         .inTable("users")
         .onDelete("CASCADE")
         .onUpdate("CASCADE")
-        
-        tbl.string("board_state");
+
+        tbl.string("turns");
     });
 };
 
--- data/migrations/20200530205005_ships-table.js
+++ data/migrations/20200530205005_ships-table.js
@@ -1,8 +1,26 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("ships", tbl => {
+        tbl.increments();
+        
+        tbl.integer("game_id")
+        .references("id")
+        .inTable("games")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+        
+        tbl.integer("player_id")
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+        
+        tbl.string("name");
+        tbl.string("start_coord");
+        tbl.string("end_coord");
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists("ships")
 };
